8a68c520d9c8f7328c8b391bb1b529a734e26819
league <liangyuanli2020@gmail.com>
2023-06-30 18:27:04 -0700

    I worked on 4 things.

--- _03_print_and_popups/_d_quiz_game.py
+++ _03_print_and_popups/_d_quiz_game.py
@@ -1,25 +1,53 @@
 from tkinter import messagebox, simpledialog, Tk
 
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
-    # Make a new window variable, window = Tk()
-    
-    # Hide the window using the window's .withdraw() method
-    
-    # 1. Create a variable to hold the user's score. Set it equal to zero. 
+if __name__ == '__main__':
 
+    # Make a new window variable, window = Tk()
+    window = Tk()
+    # Hide the window using the window's .withdraw() method
+    window.withdraw()
+    # 1. Create a variable to hold the user's score. Set it equal to zero. 
+    score = 0
     # ASK A QUESTION AND CHECK THE ANSWER
 
     #      // 2.  Ask the user a question 
-    
+    answer1 = simpledialog.askinteger(title='Questtion 1', prompt="What is 1+1?")
     #      // 3.  Use an if statement to check if their answer is correct
-
+    if answer1 == 2:
     #      // 4.  if the user's answer was correct, add one to their score 
- 
+        score += 1
     # MAKE MORE QUESTIONS. Ask more questions by repeating the above 
     #      // Option: Subtract a point from their score for a wrong answer
- 
+    answer1 = simpledialog.askinteger(title='Questtion 2', prompt="What is 2+2?")
+    #      // 3.  Use an if statement to check if their answer is correct
+    if answer1 == 4:
+    #      // 4.  if the user's answer was correct, add one to their score
+        score += 1
+        answer1 = simpledialog.askinteger(title='Questtion 3', prompt="What is 4+4?")
+    #      // 3.  Use an if statement to check if their answer is correct
+    if answer1 == 8:
+    #      // 4.  if the user's answer was correct, add one to their score
+        score += 1
+        answer1 = simpledialog.askinteger(title='Questtion 4', prompt="What is 8+8?")
+    #      // 3.  Use an if statement to check if their answer is correct
+    if answer1 == 16:
+    #      // 4.  if the user's answer was correct, add one to their score
+        score += 1
+        answer1 = simpledialog.askinteger(title='Questtion 5', prompt="What is 16+16?")
+    #      // 3.  Use an if statement to check if their answer is correct
+    if answer1 == 32:
+    #      // 4.  if the user's answer was correct, add one to their score
+        score += 1
+        answer1 = simpledialog.askinteger(title='Questtion 6', prompt="What is 32+32?")
+    #      // 3.  Use an if statement to check if their answer is correct
+    if answer1 == 64:
+    #      // 4.  if the user's answer was correct, add one to their score
+        score += 1
     # After all the questions have been asked, tell the user their final score
+
+    messagebox.showinfo(title='Final score', message="Your final score is, " + str(score) + "/6")
     # remember to convert your variable to a string using the str() function 
-    
+
     # Run the window's .mainloop() method
+    window.mainloop()
--- _03_print_and_popups/_e_awesome_or_not.py
+++ _03_print_and_popups/_e_awesome_or_not.py
@@ -2,26 +2,30 @@ from tkinter import messagebox, simpledialog, Tk
 import random
 
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
-    # Make a new window variable, window = Tk()
-    
-    # Hide the window using the window's .withdraw() method
+if __name__ == '__main__':
 
+    # Make a new window variable, window = Tk()
+    win = Tk()
+    # Hide the window using the window's .withdraw() method
+    win.withdraw()
     # 1. Make a variable equal to a positive number less than 4 using random.randInt(0, 3)
-    
+    num = random.randint(0,3)
     # 2. Print your variable to the console
-    
+    print(num)
     # 3. Get the user to enter something that they think is awesome
-    
+    thing = simpledialog.askstring(title= 'Awesome Thing', prompt= "Type something that you think is awesome.")
     # 4. If your variable is  0
+    if num == 0:
         # -- tell the user whatever they entered is awesome!
-        
+        messagebox.showinfo(title= 'Awesome', message="I think" + thing + "is awesome")
     # 5. If your variable is  1
         # -- tell the user whatever they entered is ok.
-    
+    if num == 1:
+        messagebox.showinfo(title= 'Ok', message="I think" + thing + "is ok")
     # 6. If your variable is  2
         # -- tell the user whatever they entered is boring.
-    
+    if num == 2:
+        messagebox.showinfo(title= 'Awesome', message="I think" + thing + "is awesome")
     # 7. If your variable is  3
         # -- invent your own message to give to the user (be nice).
         
