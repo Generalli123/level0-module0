1bab0c12784f49619a8692f4084d03ad3f5526a1
jereycui2008 <jeremycuid@gmail.com>
2023-07-07 18:23:58 -0700

    Made a guessing game, a cat pic, and edited a quiz.

--- _03_print_and_popups/_d_quiz_game.py
+++ _03_print_and_popups/_d_quiz_game.py
@@ -12,34 +12,34 @@ if __name__ == '__main__':
     # ASK A QUESTION AND CHECK THE ANSWER
 
     #      // 2.  Ask the user a question 
-    answer1 = simpledialog.askinteger(title='Questtion 1', prompt="What is 1+1?")
+    answer1 = simpledialog.askinteger(title='Question 1', prompt="What is 1+1?")
     #      // 3.  Use an if statement to check if their answer is correct
     if answer1 == 2:
     #      // 4.  if the user's answer was correct, add one to their score 
         score += 1
     # MAKE MORE QUESTIONS. Ask more questions by repeating the above 
     #      // Option: Subtract a point from their score for a wrong answer
-    answer1 = simpledialog.askinteger(title='Questtion 2', prompt="What is 2+2?")
+    answer1 = simpledialog.askinteger(title='Question 2', prompt="What is 2+2?")
     #      // 3.  Use an if statement to check if their answer is correct
     if answer1 == 4:
     #      // 4.  if the user's answer was correct, add one to their score
         score += 1
-        answer1 = simpledialog.askinteger(title='Questtion 3', prompt="What is 4+4?")
+        answer1 = simpledialog.askinteger(title='Question 3', prompt="What is 4+4?")
     #      // 3.  Use an if statement to check if their answer is correct
     if answer1 == 8:
     #      // 4.  if the user's answer was correct, add one to their score
         score += 1
-        answer1 = simpledialog.askinteger(title='Questtion 4', prompt="What is 8+8?")
+        answer1 = simpledialog.askinteger(title='Question 4', prompt="What is 8+8?")
     #      // 3.  Use an if statement to check if their answer is correct
     if answer1 == 16:
     #      // 4.  if the user's answer was correct, add one to their score
         score += 1
-        answer1 = simpledialog.askinteger(title='Questtion 5', prompt="What is 16+16?")
+        answer1 = simpledialog.askinteger(title='Question 5', prompt="What is 16+16?")
     #      // 3.  Use an if statement to check if their answer is correct
     if answer1 == 32:
     #      // 4.  if the user's answer was correct, add one to their score
         score += 1
-        answer1 = simpledialog.askinteger(title='Questtion 6', prompt="What is 32+32?")
+        answer1 = simpledialog.askinteger(title='Question 6', prompt="What is 32+32?")
     #      // 3.  Use an if statement to check if their answer is correct
     if answer1 == 64:
     #      // 4.  if the user's answer was correct, add one to their score
